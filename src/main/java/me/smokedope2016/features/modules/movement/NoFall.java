86e11872e5838cea4d13405aeeee7311d87ebb38
lostaccesstomygithub <ninetydm@gmail.com>
2025-05-19 15:12:57 +0200

    Deleted meteor, welcome oyvey-ported!

--- src/main/java/me/smokedope2016/features/modules/movement/NoFall.java
+++
@@ -1,9 +0,0 @@
-package me.smokedope2016.features.modules.movement;
-
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.client.MinecraftClient;
-
-public class NoFall {
-}
